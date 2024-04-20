a79b2e907753e2a8a6c3145d28ea0312966cf417
Chaeyeon1475 <chaney2225@gmail.com>
2024-04-20 22:19:22 +0900

    feat : ReviewController bookId 받아 리뷰 저장

--- src/main/java/com/rebook/review/controller/ReviewController.java
+++ src/main/java/com/rebook/review/controller/ReviewController.java
@@ -3,8 +3,8 @@ package com.rebook.review.controller;
 import com.rebook.review.domain.Review;
 import com.rebook.review.dto.ReviewRequest;
 import com.rebook.review.dto.ReviewResponse;
-import com.rebook.review.repository.ReviewRepository;
 import com.rebook.review.service.ReviewService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,26 +13,50 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.List;
 
+
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/reviews")
+@RequestMapping("/api/v1/books/{bookId}/reviews")
 @RestController
+@Tag(name = "Review", description = "Review API")
 public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping
-    public ResponseEntity<ReviewResponse> saveReview(@Valid @RequestBody final ReviewRequest reviewRequest) {
-        Review savedReview = reviewService.save(reviewRequest);
+    public ResponseEntity<ReviewResponse> saveReview(
+            @PathVariable("bookId") Long bookId,
+            @Valid @RequestBody final ReviewRequest reviewRequest){
+        Review savedReview = reviewService.save(bookId,reviewRequest);
         ReviewResponse reviewResponse = ReviewResponse.of(savedReview);
         return ResponseEntity.created(URI.create("/api/v1/reviews")).body(reviewResponse);
     }
 
     @GetMapping
-    public ResponseEntity<List<ReviewResponse>> getReviews() {
+    public ResponseEntity<List<ReviewResponse>> getReviews(){
         final List<Review> reviews = reviewService.getReviews();
         List<ReviewResponse> responses = reviews.stream()
                 .map(ReviewResponse::of)
                 .toList();
         return ResponseEntity.ok(responses);
     }
+
+    /**
+     @PostMapping("/reviews")
+     public ResponseEntity<ReviewResponse> saveReview(@Valid @RequestBody final ReviewRequest reviewRequest){
+     Review savedReview = reviewService.save(reviewRequest);
+     ReviewResponse reviewResponse = ReviewResponse.of(savedReview);
+     return ResponseEntity.created(URI.create("/api/v1/reviews")).body(reviewResponse);
+     }
+
+     @GetMapping("/reviews")
+     public ResponseEntity<List<ReviewResponse>> getReviews(){
+     final List<Review> reviews = reviewService.getReviews();
+     List<ReviewResponse> responses = reviews.stream()
+     .map(ReviewResponse::of)
+     .toList();
+     return ResponseEntity.ok(responses);
+     }
+     */
+
+
 
 }
