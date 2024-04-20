11ef732f29b3ad37e7dae5c6ac8c13e76fc9c2b6
Chaeyeon1475 <chaney2225@gmail.com>
2024-04-20 18:30:32 +0900

    test : ReviewService 수정

--- src/test/java/com/rebook/review/service/ReviewServiceTest.java
+++ src/test/java/com/rebook/review/service/ReviewServiceTest.java
@@ -1,5 +1,7 @@
 package com.rebook.review.service;
 
+import com.rebook.book.domain.BookEntity;
+import com.rebook.book.repository.BookRepository;
 import com.rebook.review.domain.Review;
 import com.rebook.review.dto.ReviewRequest;
 import org.junit.jupiter.api.DisplayName;
@@ -14,18 +16,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class ReviewServiceTest {
     @Autowired
     private ReviewService reviewService;
+    @Autowired
+    private BookRepository bookRepository;
 
-    @DisplayName("리뷰 저장 후 id 반환")
+    @DisplayName("책 id로 review 저장")
     @Test
     void saveReview(){
 
-        //given 리뷰 작성
-        ReviewRequest reviewRequest = new ReviewRequest("리뷰 서비스 저장 요청",new BigDecimal("4.5"));
+        //given
+        //책 저장
+        BookEntity savedBook = bookRepository.save(new BookEntity(null, "객체 지향", "조영호", "defalut-image.jpa"));
+        //bookId로 reviewRequest 생성
+        ReviewRequest reviewRequest = new ReviewRequest( "review for 객체지향",new BigDecimal("4.5"));
 
         //when 리뷰 저장
-        Review savedReview = reviewService.save(reviewRequest);
+        Review savedReview = reviewService.save(savedBook.getId(),reviewRequest);
 
-        //then 저장된 리뷰 아이디 확인
+        //then 저장한 책의 book Id와 저장된 리뷰의 bookId 비교
+        assertEquals(savedBook.getId(),savedReview.getBook().getId());
         assertEquals(savedReview.getContent(),reviewRequest.getContent());
         assertEquals(savedReview.getStarRate(),reviewRequest.getStarRate());
 
