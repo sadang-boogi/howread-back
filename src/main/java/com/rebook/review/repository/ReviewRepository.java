de5425c882be9ed61a8966d70ff4fc94b34a4f27
Chaeyeon1475 <chaney2225@gmail.com>
2024-04-20 18:27:07 +0900

    fix: ReviewList 날짜순 정렬 반환 구현

--- src/main/java/com/rebook/review/repository/ReviewRepository.java
+++ src/main/java/com/rebook/review/repository/ReviewRepository.java
@@ -3,5 +3,8 @@ package com.rebook.review.repository;
 import com.rebook.review.domain.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long>{
+    List<ReviewEntity> findAllByOrderByCreatedAtDesc();
 }
--- src/main/java/com/rebook/review/service/ReviewService.java
+++ src/main/java/com/rebook/review/service/ReviewService.java
@@ -1,9 +1,12 @@
 package com.rebook.review.service;
 
+import com.rebook.book.domain.BookEntity;
+import com.rebook.book.repository.BookRepository;
 import com.rebook.review.domain.Review;
 import com.rebook.review.domain.ReviewEntity;
 import com.rebook.review.dto.ReviewRequest;
 import com.rebook.review.repository.ReviewRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,21 +15,23 @@ import java.util.List;
 @Service
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+    private final BookRepository bookRepository;
 
-    public ReviewService(ReviewRepository reviewRepository) {
+    public ReviewService(ReviewRepository reviewRepository, BookRepository bookRepository) {
         this.reviewRepository = reviewRepository;
+        this.bookRepository = bookRepository;
     }
-
     @Transactional
-    public Review save(ReviewRequest reviewRequest) {
-        ReviewEntity reviewEntity = ReviewEntity.of(reviewRequest);
+    public Review save(Long bookId,ReviewRequest reviewRequest){
+        BookEntity book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("bookId not found: " + bookId));
+        ReviewEntity reviewEntity = ReviewEntity.of(book,reviewRequest);
         ReviewEntity savedReview = reviewRepository.save(reviewEntity);
-
         return Review.of(savedReview);
     }
 
-    public List<Review> getReviews() {
-        List<ReviewEntity> reviews = reviewRepository.findAll();
+    public List<Review> getReviews(){
+        List<ReviewEntity> reviews = reviewRepository.findAllByOrderByCreatedAtDesc();
         return reviews.stream()
                 .map(Review::of)
                 .toList();
