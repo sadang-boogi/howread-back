57cc5902373edfebf88027b3d9eba50b2356a0d7
Chaeyeon1475 <chaney2225@gmail.com>
2024-04-21 12:25:06 +0900

    feat: ReviewController에 Swagger 어노테이션 추가

--- src/main/java/com/rebook/review/controller/ReviewController.java
+++ src/main/java/com/rebook/review/controller/ReviewController.java
@@ -4,6 +4,8 @@ import com.rebook.review.domain.Review;
 import com.rebook.review.dto.ReviewRequest;
 import com.rebook.review.dto.ReviewResponse;
 import com.rebook.review.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,20 +17,24 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/books/{bookId}/reviews")
+@Tag(name = "Review", description = "Review API")
 @RestController
 public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping
+    @Operation(summary = "Create Review for a Book", description = "해당 책에 리뷰를 작성한다.")
     public ResponseEntity<ReviewResponse> saveReview(
             @PathVariable("bookId") Long bookId,
             @Valid @RequestBody final ReviewRequest reviewRequest){
         Review savedReview = reviewService.save(bookId,reviewRequest);
         ReviewResponse reviewResponse = ReviewResponse.from(savedReview);
-        return ResponseEntity.created(URI.create("/api/v1/reviews")).body(reviewResponse);
+        URI location = URI.create(String.format("/api/v1/books/%d/reviews/%d", bookId, savedReview.getId()));
+        return ResponseEntity.created(location).body(reviewResponse);
     }
 
     @GetMapping
+    @Operation(summary = "Get All Reviews for a Book", description = "해당 책의 작성리뷰를 조회한다.")
     public ResponseEntity<List<ReviewResponse>> getReviews(@PathVariable("bookId") Long bookId){
         final List<Review> reviews = reviewService.getReviewsWithBookId(bookId);
         List<ReviewResponse> responses = reviews.stream()
