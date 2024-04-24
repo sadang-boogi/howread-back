ea76d118912664d8d38d008b3acc94192cd55321
Chaeyeon1475 <chaney2225@gmail.com>
2024-04-24 16:41:33 +0900

    feat : Reveiw soft delete 구현

--- src/main/java/com/rebook/review/domain/ReviewEntity.java
+++ src/main/java/com/rebook/review/domain/ReviewEntity.java
@@ -6,6 +6,8 @@ import com.rebook.review.dto.ReviewRequest;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.math.BigDecimal;
 
@@ -14,6 +16,8 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "review")
 @AllArgsConstructor
+@SQLRestriction("is_deleted is false")
+@SQLDelete(sql = "UPDATE review SET is_deleted = true WHERE id = ?")
 @NoArgsConstructor
 public class ReviewEntity extends BaseEntity {
     @Id
