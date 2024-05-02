10fa93ae205ca02580e560e45c37b5069bf5da4f
ghktndyd <hwasuyong99@naver.com>
2024-05-02 21:38:17 +0900

    test: findByIds 테스트

---
+++ src/test/java/com/rebook/hashtag/repository/HashtagRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.rebook.hashtag.repository;
+
+import com.rebook.hashtag.domain.HashtagEntity;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+@ActiveProfiles("test")
+@DataJpaTest
+class HashtagRepositoryTest {
+
+    @Autowired
+    private HashtagRepository hashtagRepository;
+
+    @DisplayName("해시태그 아이디들로 해시태그 목록을 조회한다. ")
+    @Test
+    void findByIds() {
+        // given
+        HashtagEntity hashtag1 = HashtagEntity.of("어려움");
+        HashtagEntity hashtag2 = HashtagEntity.of("적절함");
+
+        hashtagRepository.saveAll(List.of(hashtag1, hashtag2));
+
+        // when
+        List<HashtagEntity> hashtags = hashtagRepository.findByIds(List.of(1L, 2L));
+
+        // then
+        assertThat(hashtags).hasSize(2)
+                .extracting("id", "name")
+                .containsExactlyInAnyOrder(
+                        tuple(1L, "어려움"),
+                        tuple(2L, "적절함")
+                );
+    }
+
+}
