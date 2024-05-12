515da56f7b01d54395d0dd99de0ffa83262b8d0d
Chaeyeon1475 <chaney2225@gmail.com>
2024-05-12 11:05:35 +0900

    fix : UserController 수정

--- src/main/java/com/rebook/user/controller/UserController.java
+++ src/main/java/com/rebook/user/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.rebook.user.controller;
 
-import com.rebook.user.repository.UserRepository;
+import com.rebook.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,5 +11,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 }
