a1b62ebd02a8500f4755ed2e2cd8e3bbbe910175
Chaeyeon1475 <chaney2225@gmail.com>
2024-05-14 19:59:34 +0900

    build : swagger bearer token 설정

--- src/main/java/com/rebook/common/config/SwaggerConfig.java
+++ src/main/java/com/rebook/common/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,17 +15,27 @@ import org.springframework.context.annotation.Configuration;
 )
 @Configuration
 public class SwaggerConfig {
-        @Bean
-        public OpenAPI openAPI() {
-                return new OpenAPI()
-                        .components(new Components())
-                        .info(apiInfo());
-        }
 
-        private Info apiInfo() {
-                return new Info()
-                        .title("reBook API 명세서")
-                        .description("rebook 프로젝트에 사용되는 API 명세서")
-                        .version("v1");
-        }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(apiInfo());
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("reBook API 명세서")
+                .description("rebook 프로젝트에 사용되는 API 명세서")
+                .version("v1");
+    }
 }
