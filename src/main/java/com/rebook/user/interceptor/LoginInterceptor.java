a4ab31f07451b88646d87c5ef8ebcf18cc9f1118
Chaeyeon1475 <chaney2225@gmail.com>
2024-05-15 10:40:51 +0900

    feat : WebConfig에 로그인 인터셉터 등록

--- src/main/java/com/rebook/user/interceptor/LoginInterceptor.java
+++ src/main/java/com/rebook/user/interceptor/LoginInterceptor.java
@@ -1,7 +1,6 @@
 package com.rebook.user.interceptor;
 
 import com.rebook.user.exception.TokenException;
-import com.rebook.user.exception.TokenExceptionCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
