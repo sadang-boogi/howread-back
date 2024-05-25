2cb5071fa0cfddf2b944677d0a2da2a627e27cb9
ghktndyd <hwasuyong99@naver.com>
2024-05-25 17:26:52 +0900

    test: 페이징 처리 정렬 조회 테스트

---
+++ src/test/java/com/rebook/book/repository/BookRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.rebook.book.repository;
+
+import com.rebook.book.domain.BookEntity;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@DataJpaTest
+class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @DisplayName("책 엔티티를 조회할 때는 생성일자를 내림차순으로 정렬하여 반환한다.")
+    @Test
+    void findAllByPageable() {
+        // given
+        List<BookEntity> bookList = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            BookEntity book = BookEntity.of(String.format("제목%d", i), String.format("저자%d", i), String.format("썸네일%d", i));
+            bookList.add(book);
+        }
+
+        bookRepository.saveAll(bookList);
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("createdAt").descending());
+
+        // when
+        Slice<BookEntity> findAllByPageable = bookRepository.findAllByPageable(pageable);
+
+        // then
+        assertThat(findAllByPageable.getContent()).hasSize(10);
+        assertThat(findAllByPageable.getContent().get(0).getCreatedAt())
+                .isAfter(findAllByPageable.getContent().get(1).getCreatedAt());
+    }
+
+}
