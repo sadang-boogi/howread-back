3371584925d8b201a3cee48676707d0722074c43
ghktndyd <hwasuyong99@naver.com>
2024-05-29 13:37:44 +0900

    refactor: 책 생성 테스트 수정

--- src/main/java/com/rebook/book/repository/BookRepository.java
+++ src/main/java/com/rebook/book/repository/BookRepository.java
@@ -6,12 +6,19 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
 
     @Query("""
             SELECT book FROM BookEntity book
             LEFT JOIN FETCH book.bookHashtags
-            WHERE book.isDeleted = false AND book.deletedAt is null
             """)
     Slice<BookEntity> findAllByPageable(Pageable pageable);
+
+    @Query("""
+            SELECT DISTINCT book FROM BookEntity book
+            LEFT JOIN FETCH book.bookHashtags bookHashtag
+            """)
+    Optional<BookEntity> findById(Long bookId);
 }
--- src/test/java/com/rebook/book/service/BookServiceTest.java
+++ src/test/java/com/rebook/book/service/BookServiceTest.java
@@ -1,17 +1,23 @@
 package com.rebook.book.service;
 
 import com.rebook.book.controller.request.BookUpdateRequest;
+import com.rebook.book.repository.BookRepository;
 import com.rebook.book.service.command.BookCreateCommand;
 import com.rebook.book.service.command.BookUpdateCommand;
 import com.rebook.book.service.dto.BookDto;
+import com.rebook.hashtag.controller.requeest.HashtagRequest;
+import com.rebook.hashtag.service.HashtagService;
+import com.rebook.hashtag.service.command.HashtagCommand;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 @SpringBootTest
@@ -20,17 +26,35 @@ class BookServiceTest {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private HashtagService hashtagService;
+
+    @BeforeEach
+    void setUp() {
+        HashtagCommand hashtagCommand1 = HashtagCommand.of(new HashtagRequest("해시태그1"));
+        HashtagCommand hashtagCommand2 = HashtagCommand.of(new HashtagRequest("해시태그2"));
+        HashtagCommand hashtagCommand3 = HashtagCommand.of(new HashtagRequest("해시태그3"));
+
+        hashtagService.create(hashtagCommand1);
+        hashtagService.create(hashtagCommand2);
+        hashtagService.create(hashtagCommand3);
+    }
+
     @DisplayName("책 제목, 저자, 썸네일 이미지, 해시태그 아이디를 등록 후 등록한 책의 id를 반환한다.")
     @Test
     void saveBookWithoutHashtag() {
         // given
-        BookCreateCommand bookCreateCommand = new BookCreateCommand("객사오", "조영호", null, null);
+        BookCreateCommand bookCreateCommand = new BookCreateCommand("객사오", "조영호", "썸네일1", List.of(1L, 2L, 3L));
 
         // when
         BookDto bookDto = bookService.save(bookCreateCommand);
 
         // then
         assertThat(bookDto.getTitle()).isEqualTo("객사오");
+        assertThat(bookDto.getHashtags().size()).isEqualTo(3);
     }
 
     @DisplayName("책의 제목, 저자, 썸네일 이미지, 해시태그 아이디로 책 정보를 수정한다.")
