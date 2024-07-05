be3003ca7baeef935f2c5044bff364c3fd64b5be
ghktndyd <hwasuyong99@naver.com>
2024-07-05 12:44:26 +0900

    feat: CorsProperties 클래스에 setter 추가

--- src/main/java/com/rebook/common/domain/CorsProperties.java
+++ src/main/java/com/rebook/common/domain/CorsProperties.java
@@ -1,11 +1,13 @@
 package com.rebook.common.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Setter
 @Getter
 @ConfigurationProperties(prefix = "app.cors")
 @Component
