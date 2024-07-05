d60e737167ae5ff171d649754f3ce942902b19be
min-ki <secreata77@gmail.com>
2024-07-05 09:29:56 +0900

    refactor(auth): 소셜로그인 인증컨트롤러 리팩터링
    - Post Request Body json 포맷으로 입력받도록 수정
    - 문서와 다른 응답코드 수정

---
+++ src/main/java/com/rebook/user/controller/request/SocialLoginRequest.java
@@ -0,0 +1,10 @@
+package com.rebook.user.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SocialLoginRequest {
+    private String code;
+}
