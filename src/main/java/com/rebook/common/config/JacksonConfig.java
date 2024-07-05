aa9d7346b089689a1ca1f8b1637aaec309085e53
ghktndyd <hwasuyong99@naver.com>
2024-07-05 12:03:55 +0900

    style: 클래스명의 일관성을 위해서 Configuration을 Config로 수정

--- src/main/java/com/rebook/common/config/JacksonConfig.java
+++ src/main/java/com/rebook/common/config/JacksonConfig.java
@@ -1,14 +1,13 @@
 package com.rebook.common.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JacksonConfiguration {
+public class JacksonConfig {
 
     @Bean
     ObjectMapper objectMapper() {
