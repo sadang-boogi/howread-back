c009367039b8f624a0618e4dadd1bfe6923490d9
ghktndyd <hwasuyong99@naver.com>
2024-07-18 18:03:58 +0900

    feat(Flyway_DDL): 추가한 엔티티들 Flyway DDL 생성

--- src/main/resources/db/migration/V5__create_studygroup_table.sql
+++ src/main/resources/db/migration/V5__create_studygroup_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE study_group
 (
     id          BIGINT AUTO_INCREMENT      NOT NULL,
     name        VARCHAR(255)               NOT NULL,
+    description VARCHAR(255)               NULL,
     max_members INT                        NOT NULL,
     leader_id   BIGINT                     NOT NULL,
     created_at  datetime(6) DEFAULT NOW(6) NULL,
@@ -13,3 +14,21 @@ CREATE TABLE study_group
 
 ALTER TABLE study_group
     ADD CONSTRAINT FK_STUDY_GROUP_ON_LEADER FOREIGN KEY (leader_id) REFERENCES users (id);
+
+CREATE TABLE study_group_member
+(
+    id             BIGINT AUTO_INCREMENT      NOT NULL,
+    study_group_id BIGINT                     NOT NULL,
+    user_id        BIGINT                     NOT NULL,
+    created_at     datetime(6) DEFAULT NOW(6) NULL,
+    updated_at     datetime(6) DEFAULT NOW(6) NULL,
+    is_deleted     BIT(1)      DEFAULT 0      NOT NULL,
+    deleted_at     datetime(6)                NULL,
+    CONSTRAINT pk_study_group_member PRIMARY KEY (id)
+);
+
+ALTER TABLE study_group_member
+    ADD CONSTRAINT FK_STUDY_GROUP_MEMBER_ON_STUDY_GROUP FOREIGN KEY (study_group_id) REFERENCES study_group (id);
+
+ALTER TABLE study_group_member
+    ADD CONSTRAINT FK_STUDY_GROUP_MEMBER_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
