6f1a899ea49c976cdd817160ba90588c1e10a7c6
ghktndyd <hwasuyong99@naver.com>
2024-07-19 11:17:54 +0900

    refactor(Flyway DDL): Flyway DDL 수정

--- src/main/resources/db/migration/V5__create_studygroup_table.sql
+++ src/main/resources/db/migration/V5__create_studygroup_table.sql
@@ -4,7 +4,6 @@ CREATE TABLE study_group
     name        VARCHAR(255)               NOT NULL,
     description VARCHAR(255)               NULL,
     max_members INT                        NOT NULL,
-    leader_id   BIGINT                     NOT NULL,
     created_at  datetime(6) DEFAULT NOW(6) NULL,
     updated_at  datetime(6) DEFAULT NOW(6) NULL,
     is_deleted  BIT(1)      DEFAULT 0      NOT NULL,
@@ -12,18 +11,16 @@ CREATE TABLE study_group
     CONSTRAINT pk_study_group PRIMARY KEY (id)
 );
 
-ALTER TABLE study_group
-    ADD CONSTRAINT FK_STUDY_GROUP_ON_LEADER FOREIGN KEY (leader_id) REFERENCES users (id);
-
 CREATE TABLE study_group_member
 (
-    id             BIGINT AUTO_INCREMENT      NOT NULL,
-    study_group_id BIGINT                     NOT NULL,
-    user_id        BIGINT                     NOT NULL,
-    created_at     datetime(6) DEFAULT NOW(6) NULL,
-    updated_at     datetime(6) DEFAULT NOW(6) NULL,
-    is_deleted     BIT(1)      DEFAULT 0      NOT NULL,
-    deleted_at     datetime(6)                NULL,
+    id             BIGINT AUTO_INCREMENT         NOT NULL,
+    study_group_id BIGINT                        NOT NULL,
+    user_id        BIGINT                        NOT NULL,
+    grade          VARCHAR(255) DEFAULT 'MEMBER' NOT NULL,
+    created_at     datetime(6)  DEFAULT NOW(6)   NULL,
+    updated_at     datetime(6)  DEFAULT NOW(6)   NULL,
+    is_deleted     BIT(1)       DEFAULT 0        NOT NULL,
+    deleted_at     datetime(6)                   NULL,
     CONSTRAINT pk_study_group_member PRIMARY KEY (id)
 );
 
