afa5ba4d7b110684924a600a687502723d26c828
chaney1475 <chaney2225@gmail.com>
2024-07-27 18:12:14 +0900

    ddl 수정

--- src/main/resources/db/migration/V6__add_image_table.sql
+++ src/main/resources/db/migration/V6__add_image_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE image
 (
     id         BIGINT AUTO_INCREMENT      NOT NULL,
-    created_at datetime(6) DEFAULT NOW(6) NULL,
-    updated_at datetime(6) DEFAULT NOW(6) NULL,
+    created_at datetime(6) DEFAULT CURRENT_TIMESTAMP(6)                                NOT NULL,
+    updated_at datetime(6) DEFAULT CURRENT_TIMESTAMP(6) on update CURRENT_TIMESTAMP(6) NOT NULL,
     is_deleted BIT(1)      DEFAULT 0      NOT NULL,
     deleted_at datetime(6)                NULL,
     url        VARCHAR(255)               NOT NULL COMMENT '이미지 주소',
