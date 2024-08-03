a771817c7516765b53e5b2d4f1f5d94e14f667d3
chaney1475 <chaney2225@gmail.com>
2024-08-04 01:45:08 +0900

    수정

--- src/main/java/com/rebook/reaction/controller/ReactionController.java
+++ src/main/java/com/rebook/reaction/controller/ReactionController.java
@@ -1,5 +1,7 @@
 package com.rebook.reaction.controller;
 
+import com.rebook.auth.annotation.Authenticated;
+import com.rebook.auth.annotation.LoginRequired;
 import com.rebook.reaction.controller.response.ReactionResponse;
 import com.rebook.reaction.domain.ReactionType;
 import com.rebook.reaction.domain.TargetType;
@@ -7,6 +9,7 @@ import com.rebook.reaction.service.ReactionService;
 import com.rebook.reaction.service.command.ReactionCommand;
 import com.rebook.reaction.service.dto.ReactionDto;
 import com.rebook.user.service.dto.AuthClaims;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,28 +23,28 @@ public class ReactionController {
         this.reactionService = reactionService;
     }
 
-    //@LoginRequired
+    @LoginRequired
     @PutMapping("/{reactionType}/{targetType}/{targetId}")
     public ResponseEntity<ReactionResponse> turnOnReaction(
             @PathVariable ReactionType reactionType,
             @PathVariable TargetType targetType,
-            @PathVariable Long targetId
+            @PathVariable Long targetId,
+            @Parameter(hidden = true) @Authenticated AuthClaims authClaims
     ) {
-        AuthClaims authClaims = new AuthClaims(1L);
         ReactionCommand command = new ReactionCommand(authClaims.getUserId(), reactionType, targetType, targetId);
         ReactionDto reactionDto = reactionService.on(command);
         ReactionResponse reactionResponse = ReactionResponse.from(reactionDto);
         return ResponseEntity.ok(reactionResponse);
     }
 
-    //@LoginRequired
+    @LoginRequired
     @DeleteMapping("/{reactionType}/{targetType}/{targetId}")
     public ResponseEntity<Void> turnOffReaction(
             @PathVariable ReactionType reactionType,
             @PathVariable TargetType targetType,
-            @PathVariable Long targetId
+            @PathVariable Long targetId,
+            @Parameter(hidden = true) @Authenticated AuthClaims authClaims
     ) {
-        AuthClaims authClaims = new AuthClaims(1L);
         ReactionCommand command = new ReactionCommand(authClaims.getUserId(), reactionType, targetType, targetId);
         reactionService.off(command);
         return ResponseEntity.noContent().build();
