75b66e7bdf0baa5a19fcb7e666686fb1465f9464
chaney1475 <chaney2225@gmail.com>
2024-08-09 21:35:16 +0900

    수정

--- src/main/java/com/rebook/RebookApplication.java
+++ src/main/java/com/rebook/RebookApplication.java
@@ -1,17 +1,11 @@
 package com.rebook;
 
 import com.rebook.common.domain.CorsProperties;
-import com.rebook.jwt.JwtUtil;
-import com.rebook.user.service.dto.AuthClaims;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import java.time.Instant;
 
 @EnableConfigurationProperties(CorsProperties.class)
 @OpenAPIDefinition(
@@ -20,19 +14,10 @@ import java.time.Instant;
         }
 )
 @SpringBootApplication
-public class RebookApplication implements CommandLineRunner {
+public class RebookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RebookApplication.class, args);
     }
 
-
-    @Autowired
-    JwtUtil jwtUtil;
-
-    @Override
-    public void run(String... args) throws Exception {
-        String token = jwtUtil.createToken(new AuthClaims(1L), Instant.now());
-        System.out.println("token = " + token);
-    }
 }
