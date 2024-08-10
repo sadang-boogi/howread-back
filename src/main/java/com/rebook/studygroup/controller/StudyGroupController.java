daa12e1513a7a5915ea6cd58ffc405bdf50c2168
min-ki <secreata77@gmail.com>
2024-08-10 19:34:39 +0900

    feat: StudyGroup 목록 조회 API 추가

--- src/main/java/com/rebook/studygroup/controller/StudyGroupController.java
+++ src/main/java/com/rebook/studygroup/controller/StudyGroupController.java
@@ -2,9 +2,11 @@ package com.rebook.studygroup.controller;
 
 import com.rebook.auth.annotation.Authenticated;
 import com.rebook.auth.annotation.LoginRequired;
+import com.rebook.common.schema.ListResponse;
 import com.rebook.studygroup.controller.request.StudyGroupCreateRequest;
 import com.rebook.studygroup.controller.response.StudyGroupResponse;
 import com.rebook.studygroup.service.StudyGroupService;
+import com.rebook.studygroup.service.dto.StudyGroupDto;
 import com.rebook.studygroup.service.dto.StudyGroupMemberDto;
 import com.rebook.user.service.dto.AuthClaims;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -33,6 +35,19 @@ public class StudyGroupController {
                 studyGroupService.createStudyGroup(studyGroupCreateRequest.toCommand(), claims.getUserId()));
 
         return ResponseEntity.created(URI.create("/api/v1/studygroups/" + studyGroupResponse.getId())).body(studyGroupResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<ListResponse<StudyGroupResponse>> getStudyGroups(
+            @RequestParam(required = false, defaultValue = "1") final int page,
+            @RequestParam(required = false, defaultValue = "10") final int pageSize
+    ) {
+        List<StudyGroupDto> studyGroups = studyGroupService.getStudyGroups(page, pageSize);
+        List<StudyGroupResponse> items = studyGroups.stream().map(
+                StudyGroupResponse::from
+        ).toList();
+        return ResponseEntity.ok()
+                .body(new ListResponse<>(items));
     }
 
     @GetMapping("/{studyGroupId}")
--- src/main/java/com/rebook/studygroup/repository/StudyGroupRepository.java
+++ src/main/java/com/rebook/studygroup/repository/StudyGroupRepository.java
@@ -1,7 +1,11 @@
 package com.rebook.studygroup.repository;
 
 import com.rebook.studygroup.domain.StudyGroupEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudyGroupRepository extends JpaRepository<StudyGroupEntity, Long> {
+
+    Page<StudyGroupEntity> findStudyGroupEntitiesBy(PageRequest pageable);
 }
