99d72389171ad8f4375196449454da29a09b986a
chaney1475 <chaney2225@gmail.com>
2024-08-10 10:00:30 +0900

    포멧팅 수정

--- src/main/java/com/rebook/auth/annotation/LoginRequired.java
+++ src/main/java/com/rebook/auth/annotation/LoginRequired.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LoginRequired {
-	boolean optional() default false;
+    boolean optional() default false;
 }
--- src/main/java/com/rebook/auth/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/rebook/auth/interceptor/LoginCheckInterceptor.java
@@ -17,47 +17,47 @@ import lombok.RequiredArgsConstructor;
 @Component
 public class LoginCheckInterceptor implements HandlerInterceptor {
 
-	private final JwtService jwtService;
+    private final JwtService jwtService;
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
-		Exception {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
+            Exception {
 
-		if (!(handler instanceof HandlerMethod)) {
-			return true;
-		}
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
 
-		HandlerMethod handlerMethod = (HandlerMethod)handler;
-		LoginRequired loginRequired = handlerMethod.getMethodAnnotation(LoginRequired.class);
-		if (loginRequired == null) {
-			// 애노테이션이 없는 경우
-			return true;
-		}
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        LoginRequired loginRequired = handlerMethod.getMethodAnnotation(LoginRequired.class);
+        if (loginRequired == null) {
+            // 애노테이션이 없는 경우
+            return true;
+        }
 
-		// Authorization 헤더에서 토큰 추출
-		String authorizationHeader = request.getHeader("Authorization");
-		boolean optional = loginRequired.optional();
+        // Authorization 헤더에서 토큰 추출
+        String authorizationHeader = request.getHeader("Authorization");
+        boolean optional = loginRequired.optional();
 
-		if (isNotExist(authorizationHeader)) {
-			if (!optional) {
-				// optional이 false -> 토큰이 없는 경우 예외 발생
-				throw new TokenException(ExceptionCode.TOKEN_INVALID);
-			} else {
-				// optional이 true -> 토큰이 없는 경우 통과
-				return true;
-			}
-		}
+        if (isNotExist(authorizationHeader)) {
+            if (!optional) {
+                // optional이 false -> 토큰이 없는 경우 예외 발생
+                throw new TokenException(ExceptionCode.TOKEN_INVALID);
+            } else {
+                // optional이 true -> 토큰이 없는 경우 통과
+                return true;
+            }
+        }
 
-		String token = jwtService.extractTokenFromHeader(authorizationHeader);
+        String token = jwtService.extractTokenFromHeader(authorizationHeader);
 
-		// 토큰이 유효한 경우 AuthClaims 추출
-		request.setAttribute("authClaims", jwtService.getClaims(token));
+        // 토큰이 유효한 경우 AuthClaims 추출
+        request.setAttribute("authClaims", jwtService.getClaims(token));
 
-		return true;
-	}
+        return true;
+    }
 
-	private boolean isNotExist(final String accessToken) {
-		return accessToken == null || accessToken.isBlank();
-	}
+    private boolean isNotExist(final String accessToken) {
+        return accessToken == null || accessToken.isBlank();
+    }
 
 }
